d69baae0a0daf10b0399a4424b3dc00ce42cecbd
Facundo Laffont <facundolaffont@gmail.com>
2022-07-27 19:40:18 -0300

    Added choseFromDropDownList.java

---
+++ choseFromDropDownList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+
+public String choseFromDropDownList(String message, String title, ImageIcon icon, ArrayList<String> dropDownList) {
+    return (String) JOptionPane.showInputDialog(
+		    null,
+		    message,
+		    title,
+		    JOptionPane.QUESTION_MESSAGE,
+		    icon,
+		    dropDownList.toArray(),
+		    null
+    );
+}
