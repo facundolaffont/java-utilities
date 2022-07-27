b4d81d2e543b3cead1d47371c09e6a684fc95f97
Facundo Laffont <facundolaffont@gmail.com>
2022-07-27 19:41:28 -0300

    Update choseFromDropDownList.java

--- choseFromDropDownList.java
+++ choseFromDropDownList.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
 public String choseFromDropDownList(String message, String title, ImageIcon icon, ArrayList<String> dropDownList) {
-    return (String) JOptionPane.showInputDialog(
-		    null,
-		    message,
-		    title,
-		    JOptionPane.QUESTION_MESSAGE,
-		    icon,
-		    dropDownList.toArray(),
-		    null
-    );
+	return (String) JOptionPane.showInputDialog(
+		null,
+		message,
+		title,
+		JOptionPane.QUESTION_MESSAGE,
+		icon,
+		dropDownList.toArray(),
+		null
+	);
 }
